fad7cd803a6b767cecddeac398a43d793a252ab7
taqdees <taqdeeskhalid84@gmail.com>
2025-05-18 23:18:59 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 
 import './App.css'
-import Counter from './Counter'
+import Password from './Password'
 
 
 
 function App() {
   return (
     <>
-     
-   <Counter/>
-     
+   
+    <Password/>
     </>
   )
 }
